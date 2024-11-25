ee5b000801475527a4090f1909dbfbf2aab6174f
DenSan83 <dennis.santillan83@gmail.com>
2024-11-25 17:00:18 -0500

    ADD 404 page

---
+++ app/Views/Templates/error404_tpl.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>DnD Tracker - Error 404</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        body {
+            background-image: url("./public/images/background_texture.png");
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center;
+            min-height: 100vh;
+            background-attachment: fixed;
+        }
+        .container {
+            display: flex   ;
+            flex-direction: column;
+            align-items: center;
+        }
+        .navbar {
+            height: 2em;
+        }
+        .main {
+            max-width: 600px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container text-center">
+        <div class="navbar w-100">
+            [Navbar]
+        </div>
+        <div class="h1">
+            Page Not Found
+        </div>
+        <div class="main">
+            <img src="./public/images/404.png" class="w-100">
+        </div>
+        <div class="text">
+            We live in a world of uncertainty.
+            But certainly, the page you were looking for isn’t here.<br>
+            Perhaps this halfling has stolen it and hidden it in another place.<br>
+            Try searching for what you were looking for in another realm.
+        </div>
+    </div>
+</body>
+</html>
